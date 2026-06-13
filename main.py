b0007955d7c79bbd678978bdeb33fa6f210598c9
myanmeportugal <fmy33689@gmail.com>
2026-06-13 19:28:13 +0600

    Updated twin

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def add(a, b):
+def add(a,b):
     return a + b
+
+def sub(a,b):
+    return a-b
+
+a=10
+b=20
+result = add(a,b)
+print("The sum of", a, "and", b, "is", result)
+result_sub = sub(a,b)
+print("The difference of", a, "and", b, "is", result_sub)
