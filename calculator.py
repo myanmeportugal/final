d4a8e624ae38d5b08f929fd7635e373f34ec3850
myanmeportugal <fmy33689@gmail.com>
2026-07-03 19:38:48 +0600

    added project's work file

--- calculator.py
+++ calculator.py
@@ -1,13 +1,31 @@
-def add(a, b):
-    return a + b
+mport main  # main.py ফাইলটিকে ইমপোর্ট করা হলো
+import unittest
 
-def subtract(a, b):
-    return a - b
 
-def multiply(a, b):
-    return a * b
+class TestCalculator(unittest.TestCase):
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero!")
-    return a / b
+    def test_add_true(self):
+        result = main.add(10, 5)
+        self.assertTrue(result == 15)
+
+    def test_sub_false(self):
+        result = main.sub(10, 5)
+        self.assertFalse(result == 100)
+
+    def test_mul_true(self):
+        result = main.mul(10, 5)
+        self.assertTrue(result == 50)
+
+    def test_div_by_zero_true(self):
+        result = main.div(10, 0)
+        self.assertTrue(result == "Invalid! Can't divide by zero!")
+
+    def test_div_by_bigger_false(self):
+        result = main.div(5, 10)
+        self.assertTrue(
+            result == "Invalid! Can't divide small number by greater number!"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-def add(a,b):
+# 1. Functions
+def add(a, b):
     return a + b
 
-def sub(a,b):
-    return a-b
 
-a=10
-b=15
+def sub(a, b):
+    return a - b
 
-result = add(a,b)
-print("The sum of", a, "and", b, "is", result)
 
-result_sub = sub(a,b)
-print("The difference of", a, "and", b, "is", result_sub)
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if b == 0:
+        return "Invalid! Can't divide by zero!"
+    elif b > a:
+        return "Invalid! Can't divide small number by greater number!"
+    return a / b
+
+
+# 2. Variables & Prints
+a = 10
+b = 15
+
+print("The sum of", a, "and", b, "is", add(a, b))
+print("The difference of", a, "and", b, "is", sub(a, b))
