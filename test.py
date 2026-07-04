5e2937bb0f541cd85e440ced1e4dd71dab0e74ae
myanmeportugal <fmy33689@gmail.com>
2026-07-04 19:55:44 +0600

    added bro added

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
 import main
 import unittest
 
+
 class TestCalculator(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(main.add(2, 3), 5)
-        self.assertEqual(main.add(-1, -5), -6)
+    def test_add_true(self):
+        result = main.add(10, 5)
+        self.assertTrue(result == 15)
+
+    def test_sub_false(self):
+        result = main.sub(10, 5)
+        self.assertFalse(result == 100)
+
+    def test_mul_true(self):
+        result = main.mul(10, 5)
+        self.assertTrue(result == 50)
+
+    def test_div_by_zero_true(self):
+        result = main.div(10, 0)
+        self.assertTrue(result == "Invalid! Can't divide by zero!")
+
+    def test_div_by_bigger_false(self):
+        result = main.div(5, 10)
+        self.assertTrue(
+            result == "Invalid! Can't divide small number by greater number!"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
